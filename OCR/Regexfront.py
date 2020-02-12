5c383774b493f0f6dd3bb7d6d5404596c9b03c14
Dev-Shankar-Paul <paulds2001@gmail.com>
2020-02-13 04:26:59 +0530

    Cleaning Data

---
+++ OCR/Regexfront.py
@@ -0,0 +1,28 @@
+import re
+
+#SAMPLE DATA [FRONT OF ID CARD]
+stringfront= """
+VIT
+VELLORE CAMPUS
+Dev Shankar Paul
+19BEE0375
+HOSTELLER
+"""
+
+def extract_names(string):
+    pattern=re.compile(r'[A-Z][a-z]+')
+    names=pattern.findall(string)
+    newname=' '.join(names)
+    return newname
+
+def extract_reg_number(string):
+    pat3=re.compile(r'[1-9][0-9][BM].+')
+    re3=pat3.findall(string)
+    re3=''.join(re3)
+    return re3
+
+names=extract_names(stringfront)
+regs=extract_reg_number(stringfront)
+
+print(names)
+print(regs)
