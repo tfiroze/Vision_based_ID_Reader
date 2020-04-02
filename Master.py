13e3bc7eb0357483865bedc250ca625a09594f29
Divyansh <divyanshkhandelwal147@gmail.com>
2020-04-03 02:37:34 +0530

    new algorithm

--- Master.py
+++ Master.py
@@ -8,12 +8,26 @@ import re
 from firebase import firebase
 import os
 import statistics
+from random import randint
+import string 
+import random 
+
+
 
 # Initialize path of the working directory and temporary file
 path = os.getcwd()+r"/data.txt"
 
 # Firebase path
 firebase = firebase.FirebaseApplication('https://vision-based-id-reader.firebaseio.com/', None)
+
+# Random Phone Number
+def random_PhoneNumber():
+    return str(randint(10000,99999))
+
+#Random Name
+def random_name():
+    res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 2))
+    return res
 
 # Regex for Registration Number
 def extract_reg_number(string):
@@ -48,19 +62,32 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    txt = tool.image_to_string(Image.fromarray(
-        frame), builder=pyocr.builders.TextBuilder())
+    txt = tool.image_to_string(Image.fromarray(frame), builder=pyocr.builders.TextBuilder())
 
     RegID = extract_reg_number(txt)
     Name = extract_names(txt)
+    PhoneNumber = extract_phoneNo(txt)
 
     # Writing the on file; Empty files should not be written
-    if Name != "":
-        if RegID != "":
-            f.write(Name)
-            f.write(",")
-            f.write(RegID)
-            f.write("\n")
+    
+
+    if PhoneNumber != "":
+        f.write(random_name())
+        f.write(",")
+        f.write(random_name())
+        f.write(",")
+        f.write(PhoneNumber)
+        f.write("\n")
+    else:
+        if Name != "":
+            if RegID != "":
+                f.write(Name)
+                f.write(",")
+                f.write(RegID)
+                f.write(",")
+                f.write(random_PhoneNumber())
+                f.write("\n")
+
 
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -77,93 +104,63 @@ if os.path.getsize(path) > 0:
         data = file.read()
 
     nameList = []           # nameList contains a list of extracted names
-    regNoList = []          # negNoList contains a list of extracted registration number
+    regNoList = []          # regNoList contains a list of extracted registration number
+    phoneNoList = []
 
+    
     # Split the string into Names and Registration numbers
-    string = re.split('\n|,', data)
+    fileData = re.split('\n|,', data)
+
     # Separate Registration numbers and other redundant data(if any) from list
-    for i in string:
-        m = re.search(r'\d+$', i)
+    for i in fileData:
+        m = re.search(r'[0-9]{10}', i)
         if(m is not None):
-            regNoList.append(i)
+            phoneNoList.append(i)
         else:
-            nameList.append(i)
+            x = re.search(r'[0-2][0-9][B|M][A-Z][A-Z][0-2][0-9][0-9][0-9]', i)
+            if(x is not None):
+                regNoList.append(i)
+            else:
+                y = re.search(r'[A-Z][a-z]+',i)
+                if(y is not None):
+                    nameList.append(i)
 
-    # print(nameList)
-    # print("*"*130)
-    # print("*"*130)
-    # print(regNoList)
-
+       
     # Get the frequency of elements and the elements as lists (in ascending order)
     regfreq = np.unique(regNoList, return_counts=True)[1].tolist()
     regval = np.unique(regNoList, return_counts=True)[0].tolist()
-    # print(regfreq)
-    # print(regval)
-    
+        
     namefreq = np.unique(nameList, return_counts=True)[1].tolist()
     nameval = np.unique(nameList, return_counts=True)[0].tolist()
-    print(namefreq)
-    print(nameval)
-
+    
+    phonefreq = np.unique(phoneNoList, return_counts=True)[1].tolist()
+    phoneval = np.unique(phoneNoList, return_counts=True)[0].tolist()
+    
     # Get the index where the maximum value is located
     maxNameIndex = regfreq.index(max(regfreq))
     maxRegIndex = namefreq.index(max(namefreq))
+    maxPhoneIndex = phonefreq.index(max(phonefreq))
 
     # Store Name and Reg Number
     ID = regval[maxNameIndex]
     Name = nameval[maxRegIndex]
+    Contact = phoneval[maxPhoneIndex]
 
-    print(ID)
-    print(Name)
-    os.remove(path)
-
-#Phone Number
-cap = cv2.VideoCapture(0)
-f = open(path, "w")
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    txt = tool.image_to_string(Image.fromarray(frame), builder=pyocr.builders.TextBuilder())
-    
-    PhoneNumber = extract_phoneNo(txt)
-    if PhoneNumber != "":
-        f.write(PhoneNumber)
-        f.write("\n")
-
-    cv2.imshow("frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        f.flush()
-        f.close()
-        break
-cap.release()
-cv2.destroyAllWindows()
-
-regs =[]
-if os.path.getsize(path) > 0:
-    with open(path, 'r') as file:
-        PhoneNumber = file.read()
-        with open(path) as f:
-            lines = [line.rstrip() for line in f]
-        contactNumber = statistics.mode(lines)
-# printing and post the filtered values
-    Creds = {"Registration Number": ID, "Name": Name,"Contact Number":contactNumber}
+    Creds = {"Registration Number": ID, "Name": Name,"Contact Number":Contact}
     print("Name:", Name)
     print("Registration Number:", ID)
-    print("Contact Number:", contactNumber)
+    print("Contact Number:", Contact)
     print("\nConfirm the details (y/n)")
+    
+    # Delete the file
+    os.remove(path)
     submit = input()
     if(submit == 'y'):
         results = firebase.post("/TestData/", Creds)
         print("Your details have been registered")
-        os.remove(path)
     else:
         print("Try Again")
-        os.remove(path)
-
 else:
     print("No ID Card was detected. Please try again")
 
-    # delete the file
-    os.remove(path)
+    
