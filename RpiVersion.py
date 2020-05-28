f4e3a2a81ac10ba9fb6af33fdedfee10cf41cfa6
Tony Jacob <tony.jacob2018@vitstudent.ac.in>
2020-05-28 22:50:22 +0530

    Implementation on Rpi

--- RpiVersion.py
+++ RpiVersion.py
@@ -10,17 +10,19 @@ import datetime
 import RPi.GPIO as GPIO
 import time
 
+#GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
 GPIO.setup(24, GPIO.OUT)  #LED to GPIO24
 print("[INFO]:GPIO Set")
 
 # Lists serve as the file
-nameList = []   
-regNoList = []          
-     
+nameList = []
+regNoList = []
+
 # Firebase path
 firebase = firebase.FirebaseApplication('https://vision-based-id-reader.firebaseio.com/', None)
+print("[INFO]:Firebase Initialized")
 
 #Regex for Registration Number
 def extract_reg_number(string):
@@ -39,7 +41,7 @@ def extract_names(string):
 # Activate Tesseract
 tools = pyocr.get_available_tools()
 tool = tools[0]
-print("[INFO]:Tesseract Set")
+print("[INFO]:Tesseract Activated")
 
 # Open Camera
 cap = cv2.VideoCapture(0)
@@ -51,7 +53,7 @@ try:
 		#Image Preprocessing
 		ret, frame = cap.read()
 		frame = cv2.flip(frame, -1)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		
 		#OCR in Action
 		txt = tool.image_to_string(Image.fromarray(gray), builder=pyocr.builders.TextBuilder())
@@ -67,7 +69,7 @@ try:
 			
 		if button_state==False:	
 			GPIO.output(24, 1)
-			print('Button Pressed...')
+			print('[INFO]:Button pressed')
 			time.sleep(0.2)
 			GPIO.output(24,0)
 			break
@@ -82,34 +84,46 @@ cv2.destroyAllWindows()
 # Get the frequency of elements and the elements as lists (in ascending order)
 regfreq = np.unique(regNoList, return_counts=True)[1].tolist()
 regval = np.unique(regNoList, return_counts=True)[0].tolist()
-    
+
 namefreq = np.unique(nameList, return_counts=True)[1].tolist()
 nameval = np.unique(nameList, return_counts=True)[0].tolist()
 
 #Failsafe
 if regNoList == [] and nameList == [] or regfreq==[] and namefreq==[]:
-    print("No ID Card Detected")
+    print("[ERR]:No ID Card Detected")
 
 #Get the index where the maximum value is located
 else:
-    maxNameIndex = regfreq.index(max(regfreq))
-    maxRegIndex = namefreq.index(max(namefreq))
-    Time = datetime.datetime.now()
+	maxNameIndex = regfreq.index(max(regfreq))
+	maxRegIndex = namefreq.index(max(namefreq))
+	Time = datetime.datetime.now()
 
     # Store Name and Reg Number
-    ID = regval[maxNameIndex]
-    Name = nameval[maxRegIndex]
-    Time = Time.strftime("%H:%M:%S %d-%m-%Y")
-    
-    #Pushing to Firebase
-    Creds = {"Time":Time,"Registration Number": ID, "Name": Name}
-    print("Name:", Name)
-    print("Registration Number:", ID)
-    print("\nConfirm the details (y/n)")
+	ID = regval[maxNameIndex]
+	Name = nameval[maxRegIndex]
+	Time = Time.strftime("%H:%M:%S %d-%m-%Y")
 
-    submit = input()
-    if(submit == 'y'):
-        results = firebase.post("/TestData/", Creds)
-        print("Your details have been registered")
-    else:
-        print("Oops! Try Again")
+    #Pushing to Firebase
+	Creds = {"Time":Time,"Registration Number": ID, "Name": Name}
+	print("Name:", Name)
+	print("Registration Number:", ID)
+	print("[INFO]:If details are correct, hold the button")
+
+	#Confirming the Details
+	button_state = GPIO.input(23)
+	for i in range(0,4):
+		time.sleep(1)
+	
+	time.sleep(2)	
+	if button_state==False:
+		GPIO.output(24, 1)
+		print('[INFO]:Posting to Firebase')
+		results = firebase.post("/Testdata/", Creds)
+		print("[INFO]:Your details have been registered")
+		time.sleep(0.2)
+		GPIO.output(24,0)
+	else: 
+		print("[ERR]:Details are incorrectly read,Please try again")
+		
+	GPIO.cleanup()
+	
